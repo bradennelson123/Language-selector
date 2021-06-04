98ffb752e8366e6e890eee0260c2d221704b2857
Braden Nelson <connornel123@outlook.com>
2021-06-04 13:46:17 -0700

    create form for language selector under javascript section

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,26 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".click").click(function() {
-    $(".developer-showing").toggle();
-    $(".developer-hidden").toggle();
+  $(".developer-showing").toggle();
+  $(".developer-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const skill1Input = $("input#skill1").val();
+    const valuesInput = $("input#values").val();
+    const abilitiesInput= $("input#abilities").val();
+    const talentsInput = $("input#talents").val();
+
+    $(".skill1").text(skill1Input);
+    $(".values").text(valuesInput);
+    $(".abilities").text(abilitiesInput);
+    $(".talents").text(talentsInput);
+  
+
+    $("#rust").show();
+    event.preventDefault();
   });
 });
