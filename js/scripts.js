57374ca73895ac1b804523c969cda62cf96b5568
Braden Nelson <connornel123@outlook.com>
2021-06-04 14:53:17 -0700

    add hide and show picture function to ruby language

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   const hoursStudying = parseInt(prompt("Study hours a day: Enter Value between 0-6"));
 
-  if (hoursStudying >3 ) {
+  if (hoursStudying >4 ) {
     $('#ruby').show();
   } 
-  else  {hoursStudying <3 
+  else {hoursStudying <4 
     $('#javascript').show();
   }
   
