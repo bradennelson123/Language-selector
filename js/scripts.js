36802eb473469703096a21215f1a152782d50973
Braden Nelson <connornel123@outlook.com>
2021-06-04 16:35:16 -0700

    edit indentation in html, scripts, and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
-  const hoursStudying = parseInt(prompt("Study hours a day: Enter Value between 0-4"));
+const hoursStudying = parseInt(prompt("Study hours a day: Enter Value between 0-4"));
 
-  if (hoursStudying >2.9 ) {
-    $('#ruby').show();
-  } 
-  else {hoursStudying <2 
-    $('#javascript').show();
-  }
-  
+if (hoursStudying >2.9 ) {
+$('#ruby').show();
+} 
+else {hoursStudying <2 
+$('#javascript').show();
+}
+
 });
 $(document).ready(function() {
-  $(".click").click(function() {
-  $(".developer-showing").toggle();
-  $(".developer-hidden").toggle();
-  });
+$(".click").click(function() {
+$(".developer-showing").toggle();
+$(".developer-hidden").toggle();
 });
-
-$(document).ready(function() {
-  $(".click").click(function() {
-  $(".frustration-showing").toggle();
-  $(".frustration-hidden").toggle();
-  });
 });
 
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
-    const skill1Input = $("input#skill1").val();
-    const valuesInput = $("input#values").val();
-    const abilitiesInput= $("input#abilities").val();
-    const talentsInput = $("input#talents").val();
+$(".click").click(function() {
+$(".frustration-showing").toggle();
+$(".frustration-hidden").toggle();
+});
+});
 
-    $(".skill1").text(skill1Input);
-    $(".values").text(valuesInput);
-    $(".abilities").text(abilitiesInput);
-    $(".talents").text(talentsInput);
-  
+$(document).ready(function() {
+$("#formOne").submit(function(event) {
+event.preventDefault();
+const skill1Input = $("input#skill1").val();
+const valuesInput = $("input#values").val();
+const abilitiesInput= $("input#abilities").val();
+const talentsInput = $("input#talents").val();
 
-    $("#rust").show();
-    event.preventDefault();
-  });
+$(".skill1").text(skill1Input);
+$(".values").text(valuesInput);
+$(".abilities").text(abilitiesInput);
+$(".talents").text(talentsInput);
+
+
+$("#rust").show();
+event.preventDefault();
+});
 });
